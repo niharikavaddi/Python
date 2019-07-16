9e81b5f6baa5ee3d2f7c1436486360e5a00d8526
VADDI LAKSHMI NEEHARIKA <36045615+niharikavaddi@users.noreply.github.com>
2019-07-16 14:15:31 +0530

    Update timediff.py

--- timediff.py
+++ timediff.py
@@ -1,10 +1,10 @@
 hr1,min1=[int(x) for x in input().split()]
 hr2,min2=[int(x) for x in input().split()]
-if hr2>hr1 and min2>=min1:
+if hr2>=hr1 and min2>=min1:
   print(hr2-hr1,min2-min1)
-elif hr2>hr1 and min1>=min2:
+elif hr2>=hr1 and min1>=min2:
   print(hr2-hr1,min1-min2)
-elif hr1>hr2 and min1>=min2:
+elif hr1>=hr2 and min1>=min2:
   print(hr1-hr2,min1-min2)
-elif hr1>hr2 and min2>=min1:
+elif hr1>=hr2 and min2>=min1:
   print(hr1-hr2,min2-min1)
